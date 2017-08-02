73f6e9f6dc273753ccbd29eb379e964c35302431
WesleyG9 <wesguas123@gmail.com>
2017-08-02 10:30:25 +0200

    Adding multiple Variables and scripts

--- Import and Compile Objects script.ps1
+++ Import and Compile Objects script.ps1
@@ -1,9 +1,15 @@
-<<<<<<< HEAD
-﻿#Variables
+﻿﻿
+#Variables
 $databaseserver = 'localhost';
-$GitFolder = 'C:\Git\Wesley';
-$Databasename = 'Demo';
+$Databasename = 'Demo Dynamics NAV (10-0)';
+$OutputFolder = 'C:\Users\Wesley\Documents\Export\Compile';
+$Path = '';
+
+#Export script
+Export-NAVApplicationObject $OutputFolder -DatabaseName $Databasename -Filter "Type=CodeUnit;ID=1" -ExportTxtSkipUnlicensed -force
+
+#Import Script
 Import-NAVApplicationObject -Path "C:\Git\Wesley\NAVW110.0\Codeunits\COD1" -DatabaseName "Demo Dynamics NAV (10-0)"
-=======
-﻿Import-NAVApplicationObject -Path "C:\Git\Wesley\NAVW110.0\Codeunits\COD1" -DatabaseName "Demo Dynamics NAV (10-0)"
->>>>>>> origin/New-Testing-Branch-WG
+
+#Compile Script
+Compile-NAVApplicationObject -path $Path -DatabaseServer $databaseserver -DatabaseName $DatabaseName -LogPath $OutputFolder -Recompile -SynchronizeSchemaChanges No 
