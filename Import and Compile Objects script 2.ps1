7c94fee68d32e036a468a25b26a5a544efcefe22
WesleyG9 <wesguas123@gmail.com>
2017-08-02 15:15:40 +0200

    blah

---
+++ Import and Compile Objects script 2.ps1
@@ -0,0 +1,9 @@
+﻿#Variables
+$databaseserver = 'localhost';
+$Databasename = 'Demo Dynamics NAV (10-0)';
+$OutputFolder = 'C:\Users\Wesley\Documents\Export\Compile';
+$importPath = 'C:\Git\Wesley\NAVW110.0\Codeunits\';
+$Path = '';
+
+#Import Script
+Import-NAVApplicationObject -Path $importPath -DatabaseName $Databasename
